a456d259a6923ca634b5d00a67301a99f33e2347
Ivan Naufal <ivannaufal7@gmail.com>
2019-04-06 22:31:53 +0700

    Update detaildokter_view.php

--- application/views/Detaildokter_view.php
+++ application/views/Detaildokter_view.php
@@ -1,4 +1,39 @@
-<?php $this->load->view('page_header')?>
+<?php $this->load->view('page_headerdokter')?>
+<div class="col-12 " style="background-image: linear-gradient(to left, rgba(0,255,255,0) 0%, #3670d2),url('<?=base_url('assets/caridokter.png') ?>')">
+        <div class="hdr-2">
+            <div aria-label="breadcrumb">
+                <ol class="breadcrumb bg-transparent">
+                    <li class="breadcrumb-item "><a href="#" class="text-white">Beranda</a></li>
+                    <li class="breadcrumb-item "><a href="#" class="text-white">Booking Jadwal Dokter</a></li>
+                </ol>
+            </div>
+            <h3 class="text-white jdl">Cari Nama Dokter atau Spesialisasi</h3>
+            <ul class="text-white list">
+                <li>Buat janji dengan dokter pilihan lebih mudah</li>
+                <li>Kemudahan sistem registrasi online tanpa antre</li>
+                <li>Gratis panduan kesehatan yang tepat untuk Anda</li>
+            </ul>
+            <table>
+                <tr>
+                <td>
+                    <p class="text-white srch">Saya mencari informasi mengenai:</p>
+
+                    <div class="seacrhIcon">
+                        <i><img src="<?= base_url('assets/search.png')?>" width="25px;" style="position:absolute; left: 190px; margin-top:15px;"></i>
+                        <input type="search" id="searchSpecialities" class="searchLocation" placeholder="Cari Dokter Spesialis atau Nama Dokter" autocomplete="off">
+                        
+                    </div>
+                </td>
+                <td>
+                    <p class="text-white lok">Lokasi:</p>
+                    <input type="search" class="searchLocation" placeholder="Semua Lokasi">
+                </td>
+                <td><button type="button" class="btn btn-warning">Cari</button></td>
+                </tr>
+
+            </table>
+        </div>
+    </div>
 <div class="d-flex flex-row flex-wrap">
   <div class="sidenav p-2 box-filter">
     <div class="kota">
