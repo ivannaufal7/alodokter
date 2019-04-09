8761ecaa2e7a9f802089dc0841fc36b908cc7653
azhaalvin <alvienzr@gmail.com>
2019-04-09 23:21:21 +0700

    add card-RS

--- application/views/CariRS.php
+++ application/views/CariRS.php
@@ -38,7 +38,7 @@
 <div class="row justify-content-md-center">
 <div class="col-md-3">
 <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
+  <img class="card-img-top" src="assets/RS-card1.jpg" alt="Card image cap">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
