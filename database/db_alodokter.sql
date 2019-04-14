9be70c12d866536c29fda9238e2c2f2056988069
Ivan Naufal <ivannaufal7@gmail.com>
2019-04-14 18:36:27 +0700

    Update db_alodokter.sql

--- database/db_alodokter.sql
+++ database/db_alodokter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 13, 2019 at 04:08 PM
+-- Generation Time: Apr 14, 2019 at 01:34 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -42,6 +42,27 @@ CREATE TABLE `tb_pasien` (
 INSERT INTO `tb_pasien` (`id_pasien`, `username`, `email`, `password`) VALUES
 (4, 'pasundan', 'tony@gmail.com', '130811dbd239c97bd9ce933de7349f20');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pertanyaan`
+--
+
+CREATE TABLE `tb_pertanyaan` (
+  `id_pertanyaan` int(11) NOT NULL,
+  `id_pasien` int(11) NOT NULL,
+  `topik` varchar(80) NOT NULL,
+  `pertanyaan` text NOT NULL,
+  `tanggal_tanya` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_pertanyaan`
+--
+
+INSERT INTO `tb_pertanyaan` (`id_pertanyaan`, `id_pasien`, `topik`, `pertanyaan`, `tanggal_tanya`) VALUES
+(7, 4, 'Keren guys', '<p>mantulll</p>', '2019-04-14 18:29:57');
+
 --
 -- Indexes for dumped tables
 --
@@ -53,6 +74,13 @@ ALTER TABLE `tb_pasien`
   ADD PRIMARY KEY (`id_pasien`);
 
 --
+-- Indexes for table `tb_pertanyaan`
+--
+ALTER TABLE `tb_pertanyaan`
+  ADD PRIMARY KEY (`id_pertanyaan`),
+  ADD KEY `id_pasien` (`id_pasien`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,7 +88,22 @@ ALTER TABLE `tb_pasien`
 -- AUTO_INCREMENT for table `tb_pasien`
 --
 ALTER TABLE `tb_pasien`
-  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
+  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `tb_pertanyaan`
+--
+ALTER TABLE `tb_pertanyaan`
+  MODIFY `id_pertanyaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_pertanyaan`
+--
+ALTER TABLE `tb_pertanyaan`
+  ADD CONSTRAINT `tb_pertanyaan_ibfk_1` FOREIGN KEY (`id_pasien`) REFERENCES `tb_pasien` (`id_pasien`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
