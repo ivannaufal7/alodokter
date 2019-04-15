fb9c7c02124b2d808b566cec4eed19f9ce412d92
Ivan Naufal <ivannaufal7@gmail.com>
2019-04-15 19:19:32 +0700

    Update db_alodokter

--- database/db_alodokter.sql
+++ database/db_alodokter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2019 at 01:34 PM
+-- Generation Time: Apr 15, 2019 at 02:17 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -25,6 +25,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_artikel`
+--
+
+CREATE TABLE `tb_artikel` (
+  `id_artikel` int(11) NOT NULL,
+  `judul_artikel` varchar(50) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `foto_artikel` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_dokter`
+--
+
+CREATE TABLE `tb_dokter` (
+  `id_dokter` int(11) NOT NULL,
+  `id_spesialis` int(11) NOT NULL,
+  `id_rumahsakit` int(11) NOT NULL,
+  `nama_dokter` varchar(40) NOT NULL,
+  `no_telp` varchar(12) NOT NULL,
+  `biaya` int(11) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `tindakan_medis` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_pasien`
 --
 
@@ -40,7 +70,8 @@ CREATE TABLE `tb_pasien` (
 --
 
 INSERT INTO `tb_pasien` (`id_pasien`, `username`, `email`, `password`) VALUES
-(4, 'pasundan', 'tony@gmail.com', '130811dbd239c97bd9ce933de7349f20');
+(4, 'pasundan', 'tony@gmail.com', '130811dbd239c97bd9ce933de7349f20'),
+(5, 'test', 'test@gmail.com', 'fb469d7ef430b0baf0cab6c436e70375');
 
 -- --------------------------------------------------------
 
@@ -61,11 +92,51 @@ CREATE TABLE `tb_pertanyaan` (
 --
 
 INSERT INTO `tb_pertanyaan` (`id_pertanyaan`, `id_pasien`, `topik`, `pertanyaan`, `tanggal_tanya`) VALUES
-(7, 4, 'Keren guys', '<p>mantulll</p>', '2019-04-14 18:29:57');
+(7, 4, 'Keren guys', '<p>When everything is gonna be&nbsp;</p>', '2019-04-14 18:29:57'),
+(9, 4, 'Umur saya berapa ?', '<p>asdasdxcxc</p>', '2019-04-15 12:26:55'),
+(12, 5, 'Bacot kau', '<p>Tetew</p>', '2019-04-15 17:51:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_rumahsakit`
+--
+
+CREATE TABLE `tb_rumahsakit` (
+  `id_rumahsakit` int(11) NOT NULL,
+  `nama_rs` varchar(50) NOT NULL,
+  `profil_rs` text NOT NULL,
+  `lokasi_rs` text NOT NULL,
+  `layanan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_spesialis`
+--
+
+CREATE TABLE `tb_spesialis` (
+  `id_spesialis` int(11) NOT NULL,
+  `nama_spesialis` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tb_artikel`
+--
+ALTER TABLE `tb_artikel`
+  ADD PRIMARY KEY (`id_artikel`);
+
+--
+-- Indexes for table `tb_dokter`
+--
+ALTER TABLE `tb_dokter`
+  ADD PRIMARY KEY (`id_dokter`),
+  ADD KEY `id_spesialis` (`id_spesialis`);
 
 --
 -- Indexes for table `tb_pasien`
@@ -81,22 +152,60 @@ ALTER TABLE `tb_pertanyaan`
   ADD KEY `id_pasien` (`id_pasien`);
 
 --
+-- Indexes for table `tb_rumahsakit`
+--
+ALTER TABLE `tb_rumahsakit`
+  ADD PRIMARY KEY (`id_rumahsakit`);
+
+--
+-- Indexes for table `tb_spesialis`
+--
+ALTER TABLE `tb_spesialis`
+  ADD PRIMARY KEY (`id_spesialis`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `tb_artikel`
+--
+ALTER TABLE `tb_artikel`
+  MODIFY `id_artikel` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tb_dokter`
+--
+ALTER TABLE `tb_dokter`
+  MODIFY `id_dokter` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `tb_pasien`
 --
 ALTER TABLE `tb_pasien`
-  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `tb_pertanyaan`
 --
 ALTER TABLE `tb_pertanyaan`
-  MODIFY `id_pertanyaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_pertanyaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `tb_rumahsakit`
+--
+ALTER TABLE `tb_rumahsakit`
+  MODIFY `id_rumahsakit` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tb_spesialis`
+--
+ALTER TABLE `tb_spesialis`
+  MODIFY `id_spesialis` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `tb_dokter`
+--
+ALTER TABLE `tb_dokter`
+  ADD CONSTRAINT `tb_dokter_ibfk_1` FOREIGN KEY (`id_spesialis`) REFERENCES `tb_spesialis` (`id_spesialis`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `tb_pertanyaan`
