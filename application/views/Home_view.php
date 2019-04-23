197e1a7d1cf3a0cc77489582f2a4b2be5dcbe18c
Ivan Naufal <ivannaufal7@gmail.com>
2019-04-23 20:42:44 +0700

    add footer at home page

--- application/views/Home_view.php
+++ application/views/Home_view.php
@@ -139,7 +139,7 @@
 </div>
 <?php endforeach; ?>
 
-	<div class="col-5 p-2 float-right iklan" style="position: absolute; bottom: 0px; left:70%; top: 20px; width: 120px;">
+	<div class="col-5 p-2 float-right iklan" style="position: absolute; bottom: 0px; left:70%; top: 20px; width: 120px; margin-bottom: 24px">
 		<img src="<?= base_url('assets/iklan.gif')?>" width="300px" style="padding-bottom: 24px">
 		<img src="<?= base_url('assets/iklan2.jpg')?>" width="300px">
 	</div>
@@ -150,4 +150,103 @@
 	<div class="d-flex justify-content-end col-8" style="margin-left: 0px;">
 	    <button class="btn btn-primary text-white p-2">Selanjutnya >></button>		
 	</div>
-<?php //$this->load->view('page_footer');?>
+
+
+
+
+<footer class="page-footer font-small blue pt-4" style="height:484px; position: relative; right: 120px; width: 115%; top: 250px;">
+  <div class="banner-footer">
+        <div class="container-footer flex-center">
+          <img class="imagephone" src="<?= base_url('assets/bannerr.png')?>" alt="">
+          <div class="text-banner">
+              <h3>Chat lebih dari 500 dokter di Aplikasi Alodokter!</h3>
+              <h4>Respons Cepat, Jawaban Akurat!</h4>
+          </div>
+          <div class="download-app">
+              <a class="storeimg" href="https://alodokter.onelink.me/1997390319?pid=Web-Dekstop-Pageapp&is_retargeting=true&af_dp=alodokter%3A%2F%2F&af_web_dp=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.alodokter.android">
+                <img src="<?= base_url('assets/googleplay.png')?>" alt="Google Play">
+              </a>
+              <br>
+              <a class="appstore" href="https://alodokter.onelink.me/1997390319?pid=Web-Dekstop-IOS-Pageapp&is_retargeting=true&af_dp=alodokter%3A%2F%2F&af_web_dp=https%3A%2F%2Fitunes.apple.com%2FID%2Fapp%2Fid1405482962">
+                <img src="<?= base_url('assets/appstore.png')?>" alt="App Store">  
+              </a>
+
+            </div>
+        </div>
+  </div>
+  <div class="footer-inner">
+    <!--footer top-->
+    <div class="footer-top">
+      <div class="footer-data-item aboutus">
+          <ul>
+              <li><a href="#!"><b>Alodokter</b></a></li>
+              <br>
+              <li><a href="#!" class="footerdesc" >Tentang Kami</a></li>
+              <li><a href="#!" class="footerdesc">Karier</a></li>
+              <li><a href="#!" class="footerdesc">Kontak Kami</a></li>
+              <li><a href="#!" class="footerdesc">Tim Editorial</a></li>
+              <li><a href="#!" class="footerdesc">Proteksi Alodokter</a></li>
+          </ul>
+      </div>
+      <!--end of about us-->
+
+      <div class="footer-data-item contact">
+          <ul>
+              <li><a href="#!"><b>Lainnya</b></a></li>
+              <br>
+              <li><a href="#!" class="footerdesc">Syarat dan Ketentuan</a></li>
+              <li><a href="#!" class="footerdesc">Privasi</a></li>
+              <li><a href="#!" class="footerdesc">Iklan</a></li>
+              <li><a href="#!" class="footerdesc">Gabung di Tim Dokter</a></li>
+              <li><a href="#!" class="footerdesc">Daftarkan Rumah Sakit Anda</a></li>
+          </ul>
+      </div>
+      <!--end of contact-->
+
+      <div class="footer-data-item social-media">
+        <ul>
+             <li style="margin-top:30px;"><a href="#!"><b>Media Social</b></a></li>
+          </ul>
+        <div class="footer-row">
+            <ul class="socicons-list">
+                <li>
+                  <a href='your_url_here' target='_blank'><span class="socicon-facebook"  ></span></a> 
+                </li>
+                  <li>
+                  <a href='your_url_here' target='_blank'><span class="socicon-twitter"></span></a> 
+                </li> 
+                <li>
+                  <a href='your_url_here' target='_blank'><span class="socicon-linkedin"></span></a> 
+                </li>   
+                <li>
+                  <a href='your_url_here' target='_blank'><span class="socicon-instagram"></span></a> 
+                </li>
+                <li>
+                  <a href='your_url_here' target='_blank'><span class="socicon-youtube"></span></a> 
+                </li>   
+            </ul>
+        </div>   
+        <p class="footer-data-tittle"><b style="color:white;">Bagian Dari Alodokter</b></p>   
+        <span>
+          <a href="https://www.alomedika.com" target="_blank" class="footer-data-link">alomedika.com</a>  
+        </span>
+        <span>
+          <a href="https://www.alomedika.com" target="_blank" class="footer-data-link">pobpad.com</a>
+        </span>
+      </div>
+      <!--end of sosmed-->
+    </div>
+    <!--end of footer top-->
+    <br>
+    <hr>
+    <!--footer bottom-->
+    <div class="footer-copyright">
+        <p>Hak Cipta © 2018 Alodokter</p>
+    </div>
+
+  </div> 
+  <!--end of footer inner-->
+
+</footer>
+</body>
+</html>
